ee3bde7464d7ba0de937db78567c43b6cebdbf5f
RITSUYA2020 <12ritsuya23@gmail.com>
2020-05-20 20:23:46 +0900

    [Start]5-5

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 		resource :favorites, only: [:create, :destroy]
 		resource :post_comments, only: [:create, :destroy]
 	end
+
+	resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    	resource :favorites, only: [:create, :destroy]
+    	resource :post_comments, only: [:create, :destroy]
+	end
 end
